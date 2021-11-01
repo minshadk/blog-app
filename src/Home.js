cc791cc8ea0117f1284313e443a436a2a1c26601
minshadk <minshadk13@gmail.com>
2021-11-02 00:27:33 +0530

    blog added

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,31 @@
+import React, { useState } from "react";
+
+import BlogList from "./BlogList";
+
 const Home = () => {
-  const hadleClick = (t) => {
-    console.log("Button Clicked");
-    console.log(t);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Welcome to REACT",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "React is simple",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 2,
+    },
+    { title: "Power full", body: "lorem ipsum...", author: "mario", id: 3 },
+    { title: "Pene Safe", body: "lorem ipsum...", author: "mario", id: 4 },
+  ]);
 
   const handleClickAgain = (name) => {
     console.log("hellow" + name);
   };
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={hadleClick}>Click me</button>
-      {/* <button
-        onClick={() => {
-          console.log("Anomynus Function");
-          handleClickAgain(" Value as Parameter");
-        }}
-      >Anomynus Function</button> */}
-      <button
-        onClick={() => {
-          handleClickAgain(" Value as Parameter");
-        }}
-      >
-        Anomynus Function
-      </button>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
